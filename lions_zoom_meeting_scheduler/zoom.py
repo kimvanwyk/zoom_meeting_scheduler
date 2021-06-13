5d646543d5e480e1a48bde3b39a948e2fcdc7bad
Kim van Wyk <kimv@sahomeloans.com>
2021-06-13 20:15:26 +0200

    Initial commit

---
+++ lions_zoom_meeting_scheduler/zoom.py
@@ -0,0 +1,74 @@
+from collections import namedtuple
+import datetime
+import os
+from random import randint
+
+from authlib.jose import jwt
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+MEETING = namedtuple("Meeting", ("join_url", "passcode"))
+
+JWT_HEADER = {"alg": "HS256", "typ": "JWT"}
+JWT_PAYLOAD = {"iss": os.getenv("ZOOM_API_KEY")}
+
+DEFAULTS = {
+    "type": 2,
+    "timezone": None,
+    "settings": {
+        "host_video": False,
+        "participant_video": False,
+        "join_before_host": True,
+        "jbh_time": 0,
+        "mute_upon_entry": True,
+        "auto_recording": "none",
+        "meeting_authentication": False,
+    },
+}
+
+
+def get_auth_headers():
+    exp = datetime.datetime.now() + datetime.timedelta(hours=1)
+    JWT_PAYLOAD["exp"] = exp
+    s = jwt.encode(JWT_HEADER, JWT_PAYLOAD, os.getenv("ZOOM_API_SECRET"))
+    return {
+        "content-type": "application/json",
+        "authorization": f"Bearer {str(s)[2:-1]}",
+    }
+
+
+def make_meeting(topic, requester, start_datetime, duration):
+    auth_headers = get_auth_headers()
+
+    passcode = randint(100000, 999999)
+    data = DEFAULTS.copy()
+    data["topic"] = topic
+    data["start_time"] = start_datetime.strftime("%Y-%m-%dT%H:%M:%S")
+    data["duration"] = duration
+    data["agenda"] = f"Requester Email: {requester}"
+    data["password"] = passcode
+
+    res = requests.post(
+        f"https://api.zoom.us/v2/users/{os.getenv('ZOOM_USER_ID')}/meetings",
+        json=data,
+        headers=auth_headers,
+    )
+    res.raise_for_status()
+
+    return MEETING(res.json()["join_url"], passcode)
+
+
+if __name__ == "__main__":
+
+    # res = requests.get(
+    #     "https://api.zoom.us/v2/users?status=active&page_size=30&page_number=1",
+    #     headers=auth_headers,
+    # )
+    # print("Active user request")
+    # print(res.json())
+
+    (url, code) = make_meeting("Test", "ME!", datetime.datetime.now(), 150)
+    print(url)
+    print(code)
